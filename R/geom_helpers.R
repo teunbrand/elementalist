2ea93da0a05a506ab6f8dfa1f97871022e528a13
teunbrand <tahvdbrand@gmail.com>
2020-07-22 22:13:22 +0200

    Abstract geom inheritance

--- R/geom_helpers.R
+++ R/geom_helpers.R
@@ -14,6 +14,88 @@ sniff_theme <- function(tries = 20) {
   theme
 }
 
+compute_element <- function(child = NULL, type = "line") {
+  # Get from global theme first
+  theme <- sniff_theme()
+  parent <- calc_element(paste0("elementalist.geom_", type), theme)
+  if (is.null(child)) {
+    elem <- parent
+  } else {
+    # Compute child when given
+    elem <- .int$combine_elements(child, parent)
+  }
+  if (length(class(elem)) < 3) {
+    child <- if (type == "line") {
+      element_line_seq(n = 1)
+    } else {
+      element_rect_seq(n = 1)
+    }
+    elem <- .int$combine_elements(child, elem)
+  }
+
+  return(elem)
+}
+
+exchange_defaults <- function(data, type = "line", element) {
+  if (inherits(element, "element_blank")) {
+    data[] <- lapply(data, unset_default)
+    return(data)
+  }
+
+  data <- as.list(data)
+  is_line <- type == "line"
+
+  # Colour
+  if ("colour" %in% names(data)) {
+    if (was_defaulted(data$colour)) {
+      data$colour <- element$colour
+    } else {
+      if (is_line) {
+        data$colour <- as_grouped_variable(alpha(data$colour, data$alpha))
+      } else {
+        data$colour <- as_grouped_variable(data$colour)
+      }
+    }
+  }
+
+  # Fill
+  if ("fill" %in% names(data)) {
+    if (was_defaulted(data$fill)) {
+      data$fill <- element$fill
+    } else {
+      if (!is_line) {
+        data$fill <- as_grouped_variable(alpha(data$fill, data$alpha))
+      } else {
+        data$fill <- as_grouped_variable(data$fill)
+      }
+    }
+  }
+
+  # Size
+  if ("size" %in% names(data)) {
+    if (was_defaulted(data$size)) {
+      data$size <- element$size
+    } else {
+      data$size <- as_grouped_variable(data$size)
+    }
+  }
+
+  # Linetype
+  if ("linetype" %in% names(data)) {
+    if (was_defaulted(data$linetype)) {
+      data$linetype <- element$linetype
+    } else {
+      data$linetype <- as_grouped_variable(data$linetype)
+    }
+  }
+
+  checked <- c("colour", "fill", "size", "linetype")
+  not_checked <- setdiff(names(data), checked)
+  data[not_checked] <- lapply(data[not_checked], unset_default)
+
+  return(data)
+}
+
 by_default <- function(x, y) {
   if (was_defaulted(x)) {
     if (is.null(y)) {
@@ -46,7 +128,7 @@ set_default <- function(x = double()) {
 }
 
 unset_default <- function(x) {
-  class(x) <- setdiff(class(x), c("defaulted", "vctrs_vctr"))
+  class(x) <- setdiff(class(x), c("defaulted", "vctrs_vctr", "grouped_variable"))
   x
 }
 
